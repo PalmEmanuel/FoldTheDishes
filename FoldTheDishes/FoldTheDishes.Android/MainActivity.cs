a4915e5f6cd92a54c871eaf38d85eaa68b1debef
Emanuel Palm <ep__palm@hotmail.com>
2021-09-13 14:18:04 +0200

    Status bar is always visible #6

--- FoldTheDishes/FoldTheDishes.Android/MainActivity.cs
+++ FoldTheDishes/FoldTheDishes.Android/MainActivity.cs
@@ -21,16 +21,18 @@ namespace FoldTheDishes.Droid
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            Window.AddFlags(WindowManagerFlags.Fullscreen | WindowManagerFlags.TurnScreenOn);
-            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
-            {
-                var stBarHeight = typeof(FormsAppCompatActivity).GetField("statusBarHeight", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
-                if (stBarHeight == null)
-                {
-                    stBarHeight = typeof(FormsAppCompatActivity).GetField("_statusBarHeight", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
-                }
-                stBarHeight?.SetValue(this, 0);
-            }
+            //// Hide status bar
+            //Window.AddFlags(WindowManagerFlags.Fullscreen | WindowManagerFlags.TurnScreenOn);
+            //if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+            //{
+            //    var stBarHeight = typeof(FormsAppCompatActivity).GetField("statusBarHeight", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+            //    if (stBarHeight == null)
+            //    {
+            //        stBarHeight = typeof(FormsAppCompatActivity).GetField("_statusBarHeight", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+            //    }
+            //    stBarHeight?.SetValue(this, 0);
+            //}
+
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
@@ -85,6 +87,7 @@ namespace FoldTheDishes.Droid
             }
             catch (System.Exception)
             {
+                base.OnBackPressed();
                 return;
             }
 
